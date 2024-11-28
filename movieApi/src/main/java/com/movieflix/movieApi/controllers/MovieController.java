17399857c9fbb7139d2fb0975c03f321ebc83a5e
AkhilT-HB <akhilhb02@gmail.com>
2024-11-28 11:31:08 +0530

    Implementing movie service and controller

---
+++ movieApi/src/main/java/com/movieflix/movieApi/controllers/MovieController.java
@@ -0,0 +1,8 @@
+package com.movieflix.movieApi.controllers;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+}
---
+++ movieApi/src/main/java/com/movieflix/movieApi/service/MovieServiceImpl.java
@@ -0,0 +1,81 @@
+package com.movieflix.movieApi.service;
+
+import com.movieflix.movieApi.dto.MovieDto;
+import com.movieflix.movieApi.entities.Movie;
+import com.movieflix.movieApi.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class MovieServiceImpl implements MovieService{
+
+    private final MovieRepository movieRepository;
+
+    private final FileService fileService;
+
+    public MovieServiceImpl(MovieRepository movieRepository, FileService fileService) {
+        this.movieRepository = movieRepository;
+        this.fileService = fileService;
+    }
+
+    @Value("${project.poster}")
+    private String path;
+
+    @Value("${base.url}")
+    private String baseUrl;
+
+    @Override
+    public MovieDto addMovie(MovieDto movieDto, MultipartFile file) throws IOException {
+
+        // 1. upload the file
+        String uploadedFileName = fileService.uploadFile(path, file);
+
+        // 2. set the value of field 'poster' as filename
+        movieDto.setPoster(uploadedFileName);
+
+        // 3. map dto to Movie object
+        Movie movie = new Movie(
+                null,
+                movieDto.getTitle(),
+                movieDto.getDirector(),
+                movieDto.getStudio(),
+                movieDto.getMovieCast(),
+                movieDto.getReleaseYear(),
+                movieDto.getPoster()
+        );
+
+        // 4. save the movie object -> saved Movie object
+        Movie savedMovie = movieRepository.save(movie);
+
+        // 5. generate the posterUrl
+        String posterUrl = baseUrl + "/file/" + uploadedFileName;
+
+        // 6. map Movie object to DTO object and return it
+        MovieDto response = new MovieDto(
+                savedMovie.getMovieId(),
+                savedMovie.getTitle(),
+                savedMovie.getDirector(),
+                savedMovie.getStudio(),
+                savedMovie.getMovieCast(),
+                savedMovie.getReleaseYear(),
+                savedMovie.getPoster(),
+                posterUrl
+        );
+
+        return response;
+    }
+
+    @Override
+    public MovieDto getMovie(Integer movieId) {
+        return null;
+    }
+
+    @Override
+    public List<MovieDto> getAllMovies() {
+        return List.of();
+    }
+}
