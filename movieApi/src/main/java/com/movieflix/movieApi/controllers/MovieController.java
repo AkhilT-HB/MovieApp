4f8fc0a274cc67e975e6fa2db83c094dca208580
AkhilT-HB <akhilhb02@gmail.com>
2024-11-28 23:19:37 +0530

    Completed the code of adding movies

--- movieApi/src/main/java/com/movieflix/movieApi/controllers/MovieController.java
+++ movieApi/src/main/java/com/movieflix/movieApi/controllers/MovieController.java
@@ -1,8 +1,41 @@
 package com.movieflix.movieApi.controllers;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.movieflix.movieApi.dto.MovieDto;
+import com.movieflix.movieApi.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
+@RequestMapping("/api/v1/movie")
 public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @PostMapping("/addMovie")
+    public ResponseEntity<MovieDto> addMovieHandler(@RequestPart MultipartFile file,
+                                                    @RequestPart String movieDto) throws IOException {
+        MovieDto dto = convertToDto(movieDto);
+
+        return new ResponseEntity<>(movieService.addMovie(dto,file), HttpStatus.CREATED);
+    }
+
+    private MovieDto convertToDto(String movieDtoStringObj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(movieDtoStringObj, MovieDto.class);
+    }
 }
--- movieApi/src/main/java/com/movieflix/movieApi/dto/MovieDto.java
+++ movieApi/src/main/java/com/movieflix/movieApi/dto/MovieDto.java
@@ -1,5 +1,6 @@
 package com.movieflix.movieApi.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
